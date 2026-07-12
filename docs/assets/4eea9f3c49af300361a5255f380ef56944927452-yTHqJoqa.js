82a7e5333153f8fd16376751c12b62197de163da
fcorbett <forrest@giraffedesign.com>
2026-07-12 16:23:16 -0700

    Update 404 and index HTML files to reference new JavaScript and CSS assets; remove outdated asset files to streamline project structure.

--- docs/assets/4eea9f3c49af300361a5255f380ef56944927452-yTHqJoqa.js
+++ docs/assets/4eea9f3c49af300361a5255f380ef56944927452-yTHqJoqa.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-B5N0Pjrm.js";/**
+import{c as e}from"./index-CwHlHFV2.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
---
+++ docs/assets/f501e2d8e220a52e831bcb8f341003090efc1d38-IaedJ8a2.js
@@ -0,0 +1,1 @@
+const e="/New32samplehomepage/assets/f501e2d8e220a52e831bcb8f341003090efc1d38-DkHHHwhs.png";export{e as i};
